6470839cd4beba9df2825da0432e92397d586512
Luis Rovirosa <luisrovirosa@gmail.com>
2014-12-16 23:11:14 +0100

    Refactor:Poker test extracted constants

--- spec/Poker-spec.js
+++ spec/Poker-spec.js
@@ -1,52 +1,51 @@
 var Poker = require('../src/Poker.js');
 
 describe("Poker", function(){
+    var poker;
+    beforeEach(function(){
+       poker = new Poker();
+    });
+
+    var BLACK_WINS = 'Black wins.';
+    var WHITE_WINS = 'White wins.';
 
     describe("Win with high card", function(){
         describe("says which color wins", function () {
             describe("when there is no same value on the highest card", function() {
                 it("As is higher than King", function () {
-                    var poker = new Poker();
                     var result = poker.play('Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH');
-                    expect(result).toContain('White wins.');
+                    expect(result).toContain(WHITE_WINS);
                 });
                 it("King is higher than Queen", function () {
-                    var poker = new Poker();
                     var result = poker.play('Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C QH');
-                    expect(result).toContain('Black wins.');
+                    expect(result).toContain(BLACK_WINS);
                 });
                 it("Queen is higher than Jack", function () {
-                    var poker = new Poker();
                     var result = poker.play('Black: 2H 3D 5S 9C JD White: 2C 3H 4S 8C QH');
-                    expect(result).toContain('White wins.');
+                    expect(result).toContain(WHITE_WINS);
                 });
                 it("Jack is higher than 10", function () {
-                    var poker = new Poker();
                     var result = poker.play('Black: 2H 3D 5S 9C JD White: 2C 3H 4S 8C TH');
-                    expect(result).toContain('Black wins.');
+                    expect(result).toContain(BLACK_WINS);
                 });
                 it("10 is higher than 9", function () {
-                    var poker = new Poker();
                     var result = poker.play('Black: 2H 3D 5S 8C 9D White: 2C 3H 4S 8C TH');
-                    expect(result).toContain('White wins.');
+                    expect(result).toContain(WHITE_WINS);
                 });
             });
             describe("when there is same value on some highest cards", function(){
                 it("Only the last value has the same value", function(){
-                    var poker = new Poker();
                     var result = poker.play('Black: 2H 3D 5S 7C 9D White: 2C 3H 4S 8C 9H');
-                    expect(result).toContain('White wins.');
+                    expect(result).toContain(WHITE_WINS);
                 });
                 it("Only the smaller value is different", function(){
-                    var poker = new Poker();
                     var result = poker.play('Black: 3H 4D 5S 7C 9D White: 2C 4H 5S 7C 9H');
-                    expect(result).toContain('Black wins.');
+                    expect(result).toContain(BLACK_WINS);
                 });
             });
         });
 
         xit("final test", function(){
-            var poker = new Poker();
             var result = poker.play('Black: 2H 3D 5S 9C KD White: 2C 3H 4S 8C AH');
             expect(result).toBe('White wins. - with high card: Ace');
         });
