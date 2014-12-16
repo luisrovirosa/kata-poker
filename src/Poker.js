e2c9f14f6030d517eaefcf4085b4f8ebede96938
Luis Rovirosa <luisrovirosa@gmail.com>
2014-12-16 22:57:14 +0100

    Play refactoring: getCardsOrdered method

--- src/Poker.js
+++ src/Poker.js
@@ -1,17 +1,21 @@
 function Poker() {
-
+    this.BLACK = 1;
+    this.WHITE = 7;
 }
 
 var cardOrder = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
 
-var compareCards = function(a, b){
+function compareCards(a, b){
     return cardOrder.indexOf(a[0]) < cardOrder.indexOf(b[0]);
-};
+}
 
-Poker.prototype.play = function(line){
+function getCardsOrdered(line, color) {
     var cards = line.split(' ');
-    var blackCards = cards.slice(1,6).sort(compareCards);
-    var whiteCards = cards.slice(7,14).sort(compareCards);
+    return cards.slice(color, color + 5).sort(compareCards);
+}
+Poker.prototype.play = function(line){
+    var blackCards = getCardsOrdered(line, this.BLACK);
+    var whiteCards = getCardsOrdered(line, this.WHITE);
 
     return compareCards(blackCards[0], whiteCards[0]) ? "White wins." : "Black wins.";
 };
