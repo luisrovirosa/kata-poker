a2f370d5fc4005f773a973c08ef97430e206a6e8
Luis Rovirosa <luisrovirosa@gmail.com>
2014-12-17 08:31:09 +0100

    Gulp now execute the tests automatically when run without having to save

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 
 
-gulp.task('default', function(){
+gulp.task('default', ['jasmine', 'watch']);
+
+gulp.task('watch', function(){
     gulp.watch(['src/**', 'spec/**'], ['jasmine']);
 });
 
 gulp.task('jasmine', function () {
-    return gulp.src(['spec/*.js'])
+    return gulp.src(['src/**', 'spec/*.js'])
         .pipe(jasmine({verbose:true, includeStackTrace: true}));
 });
